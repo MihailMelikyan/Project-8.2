ba18f5aed1a89e423c52564cdf038287a9da4268
MihailMelikyan <vip.mizhgan62@gmail.com>
2024-10-02 12:09:08 +0300

    выполнено!Исправлено!

--- main.py
+++ main.py
@@ -3,25 +3,28 @@ def personal_sum(numbers):
     result = 0
     for elem in numbers:
         for item in elem:
-            if isinstance(item,(int,float)):
+            if isinstance(item, (int, float)):
                 result += item
             else:
                 incorrect_data += 1
                 print(f'Некоректный тип данных для подсчета суммы - {item}')
-        return result,incorrect_data
+    return result, incorrect_data
+
 
 def calculate_average(*numbers):
     try:
         summa, incorrect_data = personal_sum(numbers)
-        count = len(numbers)
+        count = sum(
+            1 for num in numbers for item in num if isinstance(item, (int, float)))
         try:
-            avr = summa / (count - incorrect_data)
+            avr = summa / count if count > 0 else 0
             return avr
         except ZeroDivisionError:
             return 0
     except TypeError:
         print('в numbers записан некорректный тип данных')
         return None
+
 
 print(f'Результат 1: {calculate_average("1, 2, 3")}')  # Строка перебирается, но каждый символ - строковый тип
 print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')  # Учитываются только 1 и 3
